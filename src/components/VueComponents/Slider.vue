7ad4f599970e8b37bf5b4740718dc97b8e75f5dc
Leo Knudsen <lk22>
2023-09-24 00:33:07 +0200

    working on slider component for frontpage

---
+++ src/components/VueComponents/Slider.vue
@@ -0,0 +1,44 @@
+<script setup>
+  import {
+    Swiper,
+    SwiperSlide,
+  } from 'swiper/vue'
+
+  import 'swiper/css';
+
+  const onSwiper = (swiper) => {
+    console.log('onSwiper')
+  }
+
+  const onSlideChange = () => {
+    console.log('onSlideChange')
+  }
+
+  defineProps({
+    slidesPerView: {
+      type: Number,
+      default: 1,
+    },
+    spaceBetween: {
+      type: Number,
+      default: 50,
+    },
+    slides: {
+      type: Array,
+      default: () => []
+    }
+  })
+</script>
+
+<template>
+  <swiper
+    :slides-per-view="1"
+    :space-between="50"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  >
+    <swiper-slide v-for="slide in slides" key="slide">
+      <img :src="slide" alt="slide image"/>
+    </swiper-slide>
+  </swiper>
+</template>
