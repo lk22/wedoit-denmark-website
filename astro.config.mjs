2d84ef40aedcefedfbd82f7b923158f3014656c6
Leo Knudsen <lk22>
2023-09-27 16:38:25 +0200

    adding server configuration to push demo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig, passthroughImageService} from 'astro/config';
-
+import { defineConfig, passthroughImageService } from 'astro/config';
 import vue from "@astrojs/vue";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
   image: {
     service: passthroughImageService()
-  }
+  },
+  output: "server",
+  adapter: vercel()
 });
